c2f760e5dfdc2cd7d98111447f243c5dea98e9cb
guxiaojie <gujievivi@gmail.com>
2014-01-15 14:18:00 +0800

    add

---
+++ ParallaxForTableView/ParallaxForTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParallaxForTableView
+//
+//  Created by admin on 14-1-15.
+//  Copyright (c) 2014年 ku6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
